e27214cfaed8d07384c2d9a4215ff098ea5a7cea
Jake Hardy <jhardy3@live.com>
2016-07-04 12:55:55 +0200

    Before effed

--- cipherInJavaScript/cipher.js
+++ cipherInJavaScript/cipher.js
@@ -2,28 +2,39 @@ console.log( "b".charCodeAt(0) );  //=>  98
 console.log( "r".charCodeAt(0) );  //=> 114
 
 console.log( String.fromCharCode(95)  );  //=> "_"
-console.log( String.fromCharCode(111) );  //=> "o"
+console.log( String.fromCharCode(200) );  //=> "o"
 
 
-function shiftAround(char, shift) {
-  var charNumber = char.toUpperCase().charCodeAt(0)
-  var charCode = ((charNumber - 65 + shift) % 26) + 65
-  return String.fromCharCode(charCode)
-}
+// function shiftAround(char, shift) {
+//   var charNumber = char.toUpperCase().charCodeAt(0)
+//   var charCode = ((charNumber - 65 + shift) % 26) + 65
+//   return String.fromCharCode(charCode)
+// }
 
 function encode(word, shift) {
   var encodedWord = ''
 
   word.split('').forEach(function(letter){
-    if (letter >= 'A' && letter <= 'Z') {
-      encodedWord += shiftAround(letter, shift)
-    } else if (letter >= 'a' && letter <= 'z') {
-      encodedWord += shiftAround(letter, shift).toLowerCase()
+    var tempLetter = letter;
+    var charNumber = tempLetter.toUpperCase().charCodeAt(0);
+    var charMath = (charNumber - 65 + shift) % 26;
+
+    if (charMath < 0) {
+      var charCode = charMath + 65 + 26;
     } else {
-      encodedWord += letter
+      var charCode = charMath + 65;
+    }
+
+    if (letter >= 'A' && letter <= 'Z') {
+      encodedWord += String.fromCharCode(charCode)
+    } else if (letter >= 'a' && letter <= 'z') {
+      encodedWord += String.fromCharCode(charCode).toLowerCase();
+    } else {
+  
+      encodedWord += letter;
     }
   })
-  return encodedWord
+  return encodedWord;
 }
 
-console.log(encode("Et tu, brute?", 6));
+console.log(encode(encode("Et tu, brute?", 200), -200));
